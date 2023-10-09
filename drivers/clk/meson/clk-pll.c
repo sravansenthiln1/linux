b93b1585ffcb1f485ecfbab446425bbc3ba30a2a
sravansenthiln1 <143499241+sravansenthiln1@users.noreply.github.com>
2023-10-09 12:39:25 +0530

    Merge changes to drivers/clk/meson for ts050 integration
    Merge all changes to drivers/clkc/meson for ts050 integration from https://github.com/superna9999/linux/commit/26dc88383b14351703d3568c81a3964ade9b0f21?diff=unified#diff-d394c5b0cdfc03f33c3d984bc6398bcce464a448ac1ec88a3a753c287d581637

--- drivers/clk/meson/clk-pll.c
+++ drivers/clk/meson/clk-pll.c
@@ -286,6 +286,8 @@ static int meson_clk_pll_wait_lock(struct clk_hw *hw)
 		udelay(20);
 	} while (--delay);
 
+	pr_warn("%s: pll %s did not lock\n", __func__, clk_hw_get_name(hw));
+
 	return -ETIMEDOUT;
 }
 
@@ -430,9 +432,9 @@ static int meson_clk_pll_set_rate(struct clk_hw *hw, unsigned long rate,
 		meson_parm_write(clk->map, &pll->frac, frac);
 	}
 
-	/* If the pll is stopped, bail out now */
+	/* If the pll is stopped, bail out now 
 	if (!enabled)
-		return 0;
+		return 0;*/
 
 	ret = meson_clk_pll_enable(hw);
 	if (ret) {
